fb8b40fc1e1d65da9a53e79c65083156d30f41c3
Paul Schwarz <36771750+pavl21@users.noreply.github.com>
2024-03-26 20:46:02 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,6 +7,7 @@ if ! command -v apt-get &> /dev/null; then
 fi
 
 # Prüfe, ob das System Debian 11 ist, andernfalls auf Inkompatibilität hinweisen.
+apt install whiptail
 OS_INFO=$(cat /etc/os-release)
 DEBIAN_VERSION=$(echo "$OS_INFO" | grep 'VERSION_ID' | cut -d '"' -f 2)
 DEBIAN_PRETTY_NAME=$(echo "$OS_INFO" | grep 'PRETTY_NAME' | cut -d '"' -f 2)
@@ -20,10 +21,14 @@ if [ "$DEBIAN_VERSION" != "11" ]; then
     echo ""
     echo "Dein Linux-System '$DEBIAN_PRETTY_NAME' ist nicht mit dem Script kompatibel. Das Problem ist hierbei die Unterstützung von Whiptail, also die Oberflächengestaltung der Installation. Du kannst es gerne testen, ob das trotzdem klappt, es wurde oft gemeldet, dass man keine Auswahl treffen konnte."
     echo ""
-    echo "Aktuell funktioniert es nur mit Debian 11 einwandfrei. Wenn du fortfahren möchtest, kannst du es mit 'y' bestätigen. Ansonsten bitte mit 'n' abbrechen."
+    echo "Aktuell funktioniert es nur mit Debian 11 einwandfrei."
+
+    # Wartezeit von 5 Sekunden
+    sleep 5
 
     # Aufforderung zur Eingabe und Prüfung der Antwort
-    read -p "Möchtest du fortfahren? (y/n) " response
+    echo "Möchtest du fortfahren? (y/n) "
+    read -t 60 response
     if [ "$response" != "y" ]; then
         clear
         echo "ABBRUCH - - - - - - - - - -"
@@ -32,6 +37,7 @@ if [ "$DEBIAN_VERSION" != "11" ]; then
         exit 1
     fi
 fi
+
 
 
 
